00214a5933efc9405f9ac4717273520515ef5103
Marcos Luis <alu0100777086@ull.edu.es>
2015-03-11 19:22:09 +0000

    Removed useless tests

--- test/tests.js
+++ test/tests.js
@@ -52,20 +52,4 @@ suite('temperature', function() {
     calculate();
     assert.deepEqual(finaltable.innerHTML, '<p>\n</p><table class="center" id="result">\n<tbody><tr>                    <td>32</td>              </tr>\n<tr class="error">                    <td>25</td>                                  <td> DD</td>              </tr>\n</tbody></table>');
   });
-  /*
-  test('25e4F = 138871.1C', function() {
-    original.value = "25e4F";
-    calculate();
-    assert.deepEqual(converted.innerHTML, "138871.1 Celsius");
-  });
-  test('30e2C = 5432.0 Farenheit', function() {
-    original.value = "30e2C";
-    calculate();
-    assert.deepEqual(converted.innerHTML, "5432.0 Farenheit");
-  });
-  test('5X = error', function() {
-    original.value = "5X";
-    calculate();
-    assert.match(converted.innerHTML, /ERROR/);
-  });*/
 });
