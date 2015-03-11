b4a26d51aaaf9aa71978f8c6353a924727602a37
Marcos Luis <alu0100777086@ull.edu.es>
2015-03-11 19:13:08 +0000

    Added some tests

--- test/tests.js
+++ test/tests.js
@@ -9,8 +9,33 @@ suite('temperature', function() {
       converted = document.getElementById('converted');
     }
   });
+  
+  test('click button is calling calculate()', function() {
+  	original.value = "25";
+  	$("button").trigger("click");
+  	assert.deepEqual(finaltable.innerHTML, '<p>\n</p><table class="center" id="result">\n<tbody><tr>                    <td>25</td>              </tr>\n</tbody></table>');
+  });
+  
+  test('alert is getting showed on error', function() {
+  	original.value = "";
+  	window.alert = function() {};
+    var _savedAlert = window.alert; 
+		try{
+			var spy = sinon.spy(window, 'alert');
+			calculate();
+			sinon.assert.called(spy);
+		}finally{ 
+			window.alert = _savedAlert; 
+		}
+  });
+  
+  test('localStorage working', function() {
+  	original.value = "25";
+    calculate();
+    if (window.localStorage) assert.deepEqual(localStorage.original, original.value);
+  });
 
-  test('32F = 0C', function() {
+  test('calculate function working', function() {
     original.value = "32F";
     calculate();
     assert.deepEqual(finaltable.innerHTML, '<p>\n</p><table class="center" id="result">\n<tbody><tr>                    <td>32F</td>              </tr>\n</tbody></table>');
