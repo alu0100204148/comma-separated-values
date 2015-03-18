82f2a9b20227187edbaca8f5027c11a560d16f13
Marcos Luis <alu0100777086@ull.edu.es>
2015-03-18 20:18:47 +0000

    Added ajax

--- index.js
+++ index.js
@@ -1,104 +1,34 @@
-/*
+var _ = require('underscore');
+var $ = require('jquery');
 var express = require('express');
-var app = express();
 
-app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
+var csv = require('./server.js');
 
-
-app.get('/', function (request, response) {
-    response.render('index', { title: 'Comma Separated Value Analyzer' });
-});
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
-});
-*/
-
-
-
-
-//var _ = require('underscore');
-//var $ = require('jquery');
-var express = require('express');
 var app = express();
 var path = require('path');
-//var expressLayouts = require('express-ejs-layouts');
+var expressLayouts = require('express-ejs-layouts');
 
 app.set('port', (process.env.PORT || 5000));
-
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'ejs');
-//app.use(expressLayouts);
-
-app.use(express.static(__dirname + '/'));
-
-app.get('/', function (request, response) {
-    response.render('index', { title: 'Comma Separated Value Analyzer' });
-});
-
-app.listen(app.get('port'), function () {
-    console.log("Node app is running at localhost:" + app.get('port'));
-});
-
-
-
-
-/*
-var express = require('express');
-var app = express();
-var path = require('path');
-
-app.use(express.static('public'));
-
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-
-app.get('/', function (req, res) {
-  res.render('index', { title: 'Express' });
-})
-
-app.get('/chuchu', function (req, res) {
-  var isAjaxRequest = req.xhr;
-  console.log(isAjaxRequest);
-  if (isAjaxRequest) {
-    console.log(req.query);
-    res.send('{"answer": "Server responds: hello world!"}')
-  }
-  else {
-    res.send('not an ajax request');
-  }
-});
-
-var server = app.listen(3000, function () {
-
-  var host = server.address().address
-  var port = server.address().port
-
-  console.log('Example app listening at http://%s:%s', host, port)
-
-});
-
-*/
-/*
-var express = require('express');
-var app = express();
-
-app.set('port', (process.env.PORT || 5000));
-app.set('view engine', 'ejs');
-
-
-//app.use(express.static(__dirname + '/public'));
-app.use(express.static('/public'));
+app.use(expressLayouts);
+app.use(express.static(__dirname + '/'));
 
 app.get('/', function (request, response) {
-    response.render('index', { title: 'Comma Separated Value Analyzer' });
+   response.render('index', { title: 'Comma Separated Values' });
 });
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
+app.listen(app.get('port'), function () {
+   console.log("Node app is running at localhost:" + app.get('port'));
 });
 
-*/
+app.get('/ajaxEx/:cad', function (request, response) {
+   
+   response.send(JSON.stringify(csv.calculate(request.params.cad)));
+
+});
+
+app.get('/test/', function (request, response) {
+   response.render('index', { title: 'CSV test' });
+});
--- test/tests.js
+++ test/tests.js
@@ -1,10 +1,10 @@
 var assert = chai.assert;
 
-suite('csv', function() {
+suite('Comma Separated Values', function() {
 
   setup(function(){
     if (typeof __html__ !== 'undefined') {
-      document.body.innerHTML = __html__['test/test.html'];
+      document.body.innerHTML = __html__['tests/index.html'];
       original = document.getElementById('original');
       converted = document.getElementById('converted');
     }
@@ -12,45 +12,31 @@ suite('csv', function() {
   
   test('click button is calling calculate()', function() {
   	original.value = "25";
-  	//$("button").trigger("click");
-	calculate();
-  	assert.deepEqual(finaltable.innerHTML, '<p>\n</p><table class="center" id="result">\n<tbody><tr>                    <td>25</td>              </tr>\n</tbody></table>');
-  });
-  
-  test('alert is getting showed on error', function() {
-  	original.value = "";
-  	window.alert = function() {};
-    var _savedAlert = window.alert; 
-		try{
-			var spy = sinon.spy(window, 'alert');
-			calculate();
-			sinon.assert.called(spy);
-		}finally{ 
-			window.alert = _savedAlert; 
-		}
+  	$("button").trigger("click");
+  	assert.deepEqual(finaltable.innerHTML, '\n      <p>\n          </p><table class="center" id="result">\n               \n                   <tbody><tr class="">\n                       \n                       <td>25</td>\n                       \n                   </tr>\n               \n          </tbody></table>\n      <p></p>\n      ');
   });
   
   test('localStorage working', function() {
   	original.value = "25";
-    calculate();
+    calculateN();
     if (window.localStorage) assert.deepEqual(localStorage.original, original.value);
   });
 
   test('calculate function working on 1 value', function() {
     original.value = "32F";
-    calculate();
-    assert.deepEqual(finaltable.innerHTML, '<p>\n</p><table class="center" id="result">\n<tbody><tr>                    <td>32F</td>              </tr>\n</tbody></table>');
+    calculateN();
+    assert.deepEqual(finaltable.innerHTML, '\n      <p>\n          </p><table class="center" id="result">\n               \n                   <tbody><tr class="">\n                       \n                       <td>32F</td>\n                       \n                   </tr>\n               \n          </tbody></table>\n      <p></p>\n      ');
   });
   
   test('caculate function working on 3 values', function() {
     original.value = "32, 25, DD";
-    calculate();
-    assert.deepEqual(finaltable.innerHTML, '<p>\n</p><table class="center" id="result">\n<tbody><tr>                    <td>32</td>                                  <td> 25</td>                                  <td> DD</td>              </tr>\n</tbody></table>');
+    calculateN();
+    assert.deepEqual(finaltable.innerHTML, '\n      <p>\n          </p><table class="center" id="result">\n               \n                   <tbody><tr class="">\n                       \n                       <td>32</td>\n                       \n                       <td> 25</td>\n                       \n                       <td> DD</td>\n                       \n                   </tr>\n               \n          </tbody></table>\n      <p></p>\n      ');
   });
   
   test('caculate function working on values in different lines', function() {
     original.value = "32\n25, DD";
-    calculate();
-    assert.deepEqual(finaltable.innerHTML, '<p>\n</p><table class="center" id="result">\n<tbody><tr>                    <td>32</td>              </tr>\n<tr class="error">                    <td>25</td>                                  <td> DD</td>              </tr>\n</tbody></table>');
+    calculateN();
+    assert.deepEqual(finaltable.innerHTML, '\n      <p>\n          </p><table class="center" id="result">\n               \n                   <tbody><tr class="">\n                       \n                       <td>32</td>\n                       \n                   </tr>\n               \n                   <tr class="error">\n                       \n                       <td>25</td>\n                       \n                       <td> DD</td>\n                       \n                   </tr>\n               \n          </tbody></table>\n      <p></p>\n      ');
   });
 });
